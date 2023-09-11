701f4bd8d3b758f4a90bdc695b5a326e3417fa9b
makloufiy <yanismakloufi@gmail.com>
2023-09-11 12:07:03 +0200

    Debut TD2

---
+++ TD2/src/Controller/PublicationController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\PublicationType;
+use App\Repository\PublicationRepository;
+use App\Service\FlashMessageHelper;
+use App\Service\FlashMessageHelperInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Publication;
+
+class PublicationController extends AbstractController
+{
+    #[Route('/', name: 'feed', methods: ["GET", "POST"])]
+    public function index(FlashMessageHelperInterface $flashHelper, EntityManagerInterface $entityManager, Request $request, PublicationRepository $publicationRepo): Response
+    {
+        $publications = $publicationRepo->findAllOrderedByDate();
+
+        $publi = new Publication();
+
+        $form = $this->createForm(PublicationType::class, $publi, [
+            'method' => 'POST',
+            'action' => $this->generateURL('feed')
+        ]);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            // À ce stade, le formulaire et ses données sont valides
+            // L'objet "Exemple" a été mis à jour avec les données, il ne reste plus qu'à le sauvegarder
+            $entityManager->persist($publi);
+            $entityManager->flush();
+
+            //On redirige vers la page suivante
+            return $this->redirectToRoute('feed');
+        }
+
+        $flashHelper->addFormErrorsAsFlash($form);
+
+        return $this->render('publication/feed.html.twig', [
+            'publications' => $publications,
+            'formulaire' => $form
+        ]);
+    }
+}
---
+++ TD2/src/Service/FlashMessageHelper.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Service;
+
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class FlashMessageHelper implements FlashMessageHelperInterface
+{
+
+
+
+    public function __construct(private RequestStack $requestStack){}
+
+    public function addFormErrorsAsFlash(FormInterface $form) : void
+    {
+
+        $errors = $form->getErrors(true);
+        foreach ($errors as $error) {
+            $flashBag = $this->requestStack->getSession()->getFlashBag();
+            $flashBag->add("error", $error->getMessage());
+            //$this->addFlash("error", $error->getMessage());
+        }
+    }
+}
